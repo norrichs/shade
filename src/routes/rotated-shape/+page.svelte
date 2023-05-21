a8da9fab15fa68d71590c36b7202e9e7aa90d9c6
Ben Norrichs <ben.norrichs@gmail.com>
2023-05-20 23:09:08 -0400

    clear errors

--- src/routes/rotated-shape/+page.svelte
+++ src/routes/rotated-shape/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import ThreeRenderer from '../../components/three-renderer/ThreeRenderer.svelte';
 	import CutPattern from '../../components/cut-pattern/CutPattern.svelte';
-	import {generateRotatedShapeGeometry} from "../../lib/rotated-shape"
+	import type { Writable } from 'svelte/store';
+	import { generateRotatedShapeGeometry, type DepthCurveConfig } from '../../lib/rotated-shape';
 	import type {
 		RotatedShapeGeometryConfig,
 		ZCurveConfig,
@@ -10,18 +11,27 @@
 		BandSetConfig,
 		StrutConfig,
 		RotatedShapeLevel,
-		Strut,
+		Strut
 	} from '../../lib/rotated-shape';
 	import PathEdit from '../../components/path-edit/PathEdit.svelte';
-	import { strutConfig, curveConfig, depthCurveConfig, radialShapeConfig, levelConfig, bandConfig, renderConfig } from '../../lib/stores';
+	import {
+		strutConfig,
+		curveConfig,
+		depthCurveConfig,
+		radialShapeConfig,
+		levelConfig,
+		bandConfig,
+		renderConfig
+	} from '../../lib/stores';
 	import Controls from '../../components/controls/Controls.svelte';
 	import SelectBar from '../../components/select-bar/SelectBar.svelte';
-	import SaveControl from '../../components/save-control/SaveControl.svelte'
+	import SaveControl from '../../components/save-control/SaveControl.svelte';
 
 	let defaultZCurveConfig: ZCurveConfig = $curveConfig;
 	let defaultLevelConfig: LevelSetConfig = { ...$levelConfig };
 	let defaultBandConfig: BandSetConfig = $bandConfig;
 	let defaultStrutConfig: StrutConfig = $strutConfig;
+
 	let config: RotatedShapeGeometryConfig = {
 		shapeConfig: $radialShapeConfig,
 		depthCurveConfig: $depthCurveConfig,
@@ -29,12 +39,13 @@
 		zCurveConfig: defaultZCurveConfig,
 		bandConfig: defaultBandConfig,
 		strutConfig: defaultStrutConfig,
+		renderConfig: $renderConfig
 	};
 
 	let data = generateRotatedShapeGeometry(config);
 	let levels = data.levels;
 	let displayLevels: RotatedShapeLevel[];
-	let struts: Strut[]
+	let struts: Strut[];
 	let bands = data.bands;
 	let showControl: { name: string; value: unknown };
 
@@ -46,14 +57,12 @@
 			zCurveConfig: $curveConfig,
 			depthCurveConfig: $depthCurveConfig,
 			bandConfig: $bandConfig,
-			strutConfig: $strutConfig,
+			strutConfig: $strutConfig
 		};
 
-		console.debug("udpateGeometry",config.shapeConfig)
-		data = generateRotatedShapeGeometry(config)
-	}
-
-
+		console.debug('udpateGeometry', config.shapeConfig);
+		data = generateRotatedShapeGeometry(config);
+	};
 
 	$: {
 		config = {
@@ -63,28 +72,46 @@
 			zCurveConfig: $curveConfig,
 			depthCurveConfig: $depthCurveConfig,
 			bandConfig: $bandConfig,
-			strutConfig: $strutConfig,
+			strutConfig: $strutConfig
 		};
 		// console.debug(' PAGE CONFIG', config);
 		data = generateRotatedShapeGeometry(config);
 		levels = data.levels;
-		if ($renderConfig.ranges?.rangeStyle === "slice") {
-			const {levelStart, levelCount} = $renderConfig.ranges
-			displayLevels = levels.slice(levelStart || 0, levelCount ? (levelStart || 0) + levelCount : undefined)
+		if ($renderConfig.ranges?.rangeStyle === 'slice') {
+			const { levelStart, levelCount } = $renderConfig.ranges;
+			displayLevels = levels.slice(
+				levelStart || 0,
+				levelCount ? (levelStart || 0) + levelCount : undefined
+			);
 		} else {
-			displayLevels = levels
+			displayLevels = levels;
 		}
 		bands = data.bands;
 		struts = data.struts;
+	}
+	type ShowControlCurveValue =
+		| { type: 'RadialShapeConfig'; configStore: Writable<RadialShapeConfig> }
+		| { type: 'DepthCurveConfig'; configStore: Writable<DepthCurveConfig> }
+		| { type: 'ZCurveConfig'; configStore: Writable<ZCurveConfig> }
+
+	const isShowControlCurveValue = (value: ShowControlCurveValue | unknown): value is ShowControlCurveValue => {
+		return !!value && typeof value === "object" && Object.hasOwn(value, "type")
+	}
+	
+	const getPathEditCurve = (showControlValue: ShowControlCurveValue | unknown) => {
+		if (isShowControlCurveValue(showControlValue)) {
+			return showControlValue.configStore
+		}
+		return undefined;
 	}
 </script>
 
 <main>
 	<section class="container three">
-		<ThreeRenderer rslevels={levels} rsbands={bands} struts={struts}/>
+		<ThreeRenderer rslevels={levels} rsbands={bands} {struts} />
 	</section>
 	<section class="container svg">
-		<CutPattern rslevels={displayLevels} rsbands={bands} struts={struts}/>
+		<CutPattern rslevels={displayLevels} rsbands={bands} {struts} />
 	</section>
 	<section class="container controls">
 		<header>
@@ -92,26 +119,29 @@
 				bind:value={showControl}
 				options={[
 					{ name: 'None' },
-					{ name: 'Zcurve', value: curveConfig },
-					{ name: 'DepthCurve', value: depthCurveConfig},
-					{ name: 'Shape', value: radialShapeConfig },
+					{ name: 'Zcurve', value: { type: 'ZCurveConfig', configStore: curveConfig } },
+					{
+						name: 'DepthCurve',
+						value: { type: 'DepthCurveConfig', configStore: depthCurveConfig }
+					},
+					{ name: 'Shape', value: { type: 'RadialShapeConfig', configStore: radialShapeConfig } },
 					{ name: '3D' },
 					{ name: 'Levels' },
 					{ name: 'Struts' },
-					{ name: 'Cut'},
-					{ name: 'Save' },
+					{ name: 'Cut' },
+					{ name: 'Save' }
 				]}
 			/>
 		</header>
 		<div class="group">
 			<div>
 				<!-- <div>{showControl?.name}</div> -->
-				{#if showControl?.name === 'Zcurve' || showControl?.name === 'Shape' || showControl?.name === 'DepthCurve'}
-					<PathEdit curveStore={showControl.value} />
+				{#if ["Zcurve", "Shape", "DepthCurve"].includes(showControl?.name)}
+					<PathEdit curveStore={getPathEditCurve(showControl.value)} />
 				{/if}
 			</div>
 			<Controls showControl={showControl?.name} />
-			<SaveControl show={showControl?.name === "Save"} config={config} update={updateGeometry}/>
+			<SaveControl show={showControl?.name === 'Save'} {config} update={updateGeometry} />
 		</div>
 	</section>
 </main>
